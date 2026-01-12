2a3b3784cf00d97659341fe85f2d1cae64f977e4
冯智麒 <13476695+feng-zhi-qi@user.noreply.gitee.com>
2026-01-12 21:28:26 +0800

    The first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/YGO_build/',  // GitHub Pages 子目录部署必须设置
   plugins: [vue()],
   resolve: {
     alias: {
