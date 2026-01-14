98b4484dd47cd96124b8dca5956e44a662f71329
冯智麒 <13476695+feng-zhi-qi@user.noreply.gitee.com>
2026-01-14 21:54:43 +0800

    修改:移除打包相关设置

--- vite.config.ts
+++ vite.config.ts
@@ -12,16 +12,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // 可选：优化打包配置（针对生产环境）
-  // build: {
-  //   // 禁用代码拆分，确保所有 JS 合并为一个（插件会进一步内联到 HTML）
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks: () => 'singleChunk.js'
-  //     }
-  //   },
-  //   // 禁用资源映射文件（可选，减少冗余）
-  //   sourcemap: false
-  // }
+  optimizeDeps: {
+    exclude: ['@ygo_build/calc']
+  },
 })
 
